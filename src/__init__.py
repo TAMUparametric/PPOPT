d8554d2d0d06dcda9055417b09cf0b9b0b6f7990
Dustin Kenefake <Dustin.Kenefake@gmail.com>
2024-05-01 21:11:33 -0500

    set environmental variables to fix memory problem with blas libraries

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,8 @@
-"""Main __init__.py for the ppopt package. TODO: Fill in."""
+"""Main __init__.py for the ppopt package"""
+import os
+
+os.environ["OMP_NUM_THREADS"] = "1"  # export OMP_NUM_THREADS=1
+os.environ["OPENBLAS_NUM_THREADS"] = "1"  # export OPENBLAS_NUM_THREADS=1
+os.environ["MKL_NUM_THREADS"] = "1"  # export MKL_NUM_THREADS=1
+os.environ["VECLIB_MAXIMUM_THREADS"] = "1"  # export VECLIB_MAXIMUM_THREADS=1
+os.environ["NUMEXPR_NUM_THREADS"] = "1"  # export NUMEXPR_NUM_THREADS=1
