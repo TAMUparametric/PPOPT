9cdb2c8826d6a097377afdbc195cc222712f9822
Dustin Kenefake <Dustin.Kenefake@tamu.edi>
2024-05-13 14:04:59 -0500

    add thread limit to common blas libraries

--- src/ppopt/__init__.py
+++ src/ppopt/__init__.py
@@ -1,1 +1,9 @@
 """PPOPT INIT FILE"""
+"""Main __init__.py for the ppopt package"""
+import os
+
+os.environ["OMP_NUM_THREADS"] = "1"  # export OMP_NUM_THREADS=1
+os.environ["OPENBLAS_NUM_THREADS"] = "1"  # export OPENBLAS_NUM_THREADS=1
+os.environ["MKL_NUM_THREADS"] = "1"  # export MKL_NUM_THREADS=1
+os.environ["VECLIB_MAXIMUM_THREADS"] = "1"  # export VECLIB_MAXIMUM_THREADS=1
+os.environ["NUMEXPR_NUM_THREADS"] = "1"  # export NUMEXPR_NUM_THREADS=1
