84456264ea41ac7903ce55ab0f5353bbc20d2f6d
Gibraan Rahman <gibsramen@gmail.com>
2024-10-16 19:09:13 -0700

    Update logging location

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,4 @@
 """Main __init__.py for the ppopt package"""
-import logging
 import os
 
 os.environ["OMP_NUM_THREADS"] = "1"  # export OMP_NUM_THREADS=1
@@ -7,9 +6,3 @@ os.environ["OPENBLAS_NUM_THREADS"] = "1"  # export OPENBLAS_NUM_THREADS=1
 os.environ["MKL_NUM_THREADS"] = "1"  # export MKL_NUM_THREADS=1
 os.environ["VECLIB_MAXIMUM_THREADS"] = "1"  # export VECLIB_MAXIMUM_THREADS=1
 os.environ["NUMEXPR_NUM_THREADS"] = "1"  # export NUMEXPR_NUM_THREADS=1
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-
-sh = logging.StreamHandler()
-logger.addHandler(sh)
