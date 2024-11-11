c27548988e9f40b9c870522addd54a18a11e877f
Adrian Lipow <adrian.lipow@rwth-aachen.de>
2024-11-11 16:23:05 -0600

    Started refactoring redundant constraint removal

--- src/ppopt/utils/symbolic_utils.py
+++ src/ppopt/utils/symbolic_utils.py
@@ -1,10 +1,12 @@
 import sympy
 import gurobipy
 
-from typing import List
+from itertools import combinations
+
+from typing import List, Dict, Tuple
 import re
 
-# Deprecated, used sympy to check if any constraints are redundant but was not able to catch them all
+# # Deprecated, used sympy to check if any constraints are redundant but was not able to catch them all
 # def reduce_redundant_symbolic_constraints(constraints: List[sympy.core.relational.LessThan]) -> List[sympy.core.relational.LessThan]:
 #     """
 #     Reduces the constraints to a minimal set of constraints that are not redundant.
@@ -12,6 +14,9 @@ import re
 #     :param constraints: a list of symbolic constraints
 #     :return: a list of constraints that are not redundant
 #     """
+
+#     # first, we need to remove any duplicate constraints
+#     constraints = remove_duplicate_symbolic_constraints(constraints)
 
 #     constraint_list = []
 
@@ -25,6 +30,127 @@ import re
 #     return constraint_list
 
 
+def replace_square_roots_dictionary(constraint_strings: List[str]) -> Tuple[Dict[str, Tuple[str, str]], List[str], int]:
+    """
+    Replaces square root terms in a list of constraint strings with auxiliary variables.
+
+    :param constraint_strings: a list of constraint strings
+    :return: a dictionary of replacements and a list of constraint strings with square root terms replaced
+    """
+
+    counter = 0
+    replacement_dict = {} # this stores the replacement for each square root term, so that we can re-use the same aux variables where appropriate instead of creating new ones each time
+    for i_con, cs in enumerate(constraint_strings):
+        for term in re.findall(r'sqrt\((.*?)\)', cs):
+            if term not in replacement_dict:
+                replacement_dict[term] = tuple((f'aux{counter}', f'aux{counter + 1}'))
+                counter += 2
+            constraint_strings[i_con] = cs.replace(f'sqrt({term})', replacement_dict[term][0])
+
+    return replacement_dict, constraint_strings, counter
+
+
+def remove_duplicate_symbolic_constraints(constraints: List[sympy.core.relational.LessThan]) -> List[sympy.core.relational.LessThan]:
+    """
+    Removes duplicate constraints from a list of constraints.
+
+    :param constraints: a list of symbolic constraints
+    :return: a list of constraints with duplicates removed
+    """
+    unique_constraints = []
+    for c in constraints:
+        unique = True
+        for uc in unique_constraints:
+            if sympy.simplify((c.lhs - c.rhs) - (uc.lhs - uc.rhs)) == 0:
+                unique = False
+                break
+        if unique:
+            unique_constraints.append(c)
+
+    return unique_constraints
+
+
+def simplify_univariate_symbolic_constraints(constraints: List[sympy.core.relational.LessThan]) -> List[sympy.core.relational.LessThan]:
+    """
+    Simplifies univariate constraints.
+
+    :param constraints: a list of symbolic constraints
+    :return: a list of simplified constraints
+    """
+
+    simplified_constraints = []
+    for c in constraints:
+        if len(c.free_symbols) == 1:
+            simplified = sympy.solve(c, c.free_symbols.pop())
+            if isinstance(simplified, sympy.And):
+                for s in simplified.args:
+                    simplified_constraints.append(s)
+            else:
+                simplified_constraints.append(simplified)
+        else:
+            simplified_constraints.append(c)
+
+    return simplified_constraints
+
+
+def simplfiy_trivial_symbolic_constraints(constraints: List[sympy.core.relational.LessThan]) -> List[sympy.core.relational.LessThan]:
+    """
+    Simplifies trivial constraints by trying to see if some sympy simplifications result in a True value.
+
+    :param constraints: a list of symbolic constraints
+    :return: a list of simplified constraints
+    """
+
+    simplified_constraints = []
+    for c in constraints:
+        if sympy.factor(c) == True:
+            continue
+        if sympy.simplify(c) == True:
+            continue
+        if sympy.expand(c) == True:
+            continue
+        else:
+            simplified_constraints.append(c)
+
+    return simplified_constraints
+
+
+def build_gurobi_model_with_square_roots(constraint_strings: List[str], syms: List[sympy.Symbol], replacement_dict: Dict[str, Tuple[str, str]], num_aux: int) -> gurobipy.Model:
+    """
+    Builds a Gurobi model with square root terms replaced by auxiliary variables.
+
+    :param constraint_strings: a list of constraint strings
+    :param syms: a list of symbols
+    :return: a Gurobi model
+    """
+
+    model = gurobipy.Model()
+    model.setParam('OutputFlag', 0)
+
+    for s in syms:
+        model.addVar(vtype=gurobipy.GRB.CONTINUOUS, lb=-gurobipy.GRB.INFINITY, ub=gurobipy.GRB.INFINITY, name=str(s))
+    for i in range(num_aux):
+        model.addVar(vtype=gurobipy.GRB.CONTINUOUS, lb=0, ub=gurobipy.GRB.INFINITY, name=f'aux{i}')
+
+    # Build the model using a string, this way we can access all variables by name and use the constraint syntax from the strings
+    exec_string = ''
+    for v in syms:
+        exec_string += str(v) + ' = model.getVarByName("' + str(v) + '")\n'
+    for key, value in replacement_dict.items():
+        exec_string += f"{value[0]} = model.getVarByName('{value[0]}')\n"
+        exec_string += f"{value[1]} = model.getVarByName('{value[1]}')\n"
+        exec_string += f"model.addConstr({key} == {value[1]})\n"
+        exec_string += f"model.addGenConstrPow({value[1]}, {value[0]}, 0.5)\n"
+    for constr in constraint_strings:
+        exec_string += 'model.addConstr(' + constr + ')\n'
+
+    model.update() # essential to get all information
+
+    exec(exec_string)
+
+    return model
+
+
 # TODO this needs to be refactored later, I'm just going to build a gurobi model in here for now
 def reduce_redundant_symbolic_constraints(constraints: List[sympy.core.relational.LessThan]) -> List[sympy.core.relational.LessThan]:
     """
@@ -34,6 +160,15 @@ def reduce_redundant_symbolic_constraints(constraints: List[sympy.core.relationa
     :return: a list of symbolic constraints that are not redundant
     """
 
+    # first, we need to simplify any univariate constraints
+    constraints = simplify_univariate_symbolic_constraints(constraints)
+
+    # next, we need to simplify any trivial constraints
+    constraints = simplfiy_trivial_symbolic_constraints(constraints)
+
+    # next, we need to remove any duplicate constraints
+    constraints = remove_duplicate_symbolic_constraints(constraints)
+
     # TODO this can probably be more efficient
     
     # Gurobi can't directly handle square roots, so we need to replace them with auxiliary variables
@@ -42,14 +177,11 @@ def reduce_redundant_symbolic_constraints(constraints: List[sympy.core.relationa
     # We add the constraint expr(theta) = aux_(i+1)
     # We add the constraint aux_i = aux_(i+1)^0.5
     constraint_strings = [str(c) for c in constraints]
-    counter = 0
-    replacement_dict = {} # this stores the replacement for each square root term, so that we can re-use the same aux variables where appropriate instead of creating new ones each time
-    for i_con, cs in enumerate(constraint_strings):
-        for term in re.findall(r'sqrt\((.*?)\)', cs):
-            if term not in replacement_dict:
-                replacement_dict[term] = tuple((f'aux{counter}', f'aux{counter + 1}'))
-                counter += 2
-            constraint_strings[i_con] = cs.replace(f'sqrt({term})', replacement_dict[term][0])
+    replacement_dict, constraint_strings, num_aux = replace_square_roots_dictionary(constraint_strings)
+
+    for i, c in enumerate(constraints):
+        if isinstance(c, sympy.Equality):
+            constraint_strings[i] = str(c.lhs - c.rhs) + ' == 0'
 
     nonredundant_constraint_list = []
     syms = []
@@ -67,29 +199,7 @@ def reduce_redundant_symbolic_constraints(constraints: List[sympy.core.relationa
 
         constraint_strings[i_con] = c.replace('<=', '==')
 
-        model = gurobipy.Model()
-        model.setParam('OutputFlag', 0)
-
-        for s in syms:
-            model.addVar(vtype=gurobipy.GRB.CONTINUOUS, lb=-gurobipy.GRB.INFINITY, ub=gurobipy.GRB.INFINITY, name=str(s))
-        for i in range(counter):
-            model.addVar(vtype=gurobipy.GRB.CONTINUOUS, lb=0, ub=gurobipy.GRB.INFINITY, name=f'aux{i}')
-
-        # Build the model using a string, this way we can access all variables by name and use the constraint syntax from the strings
-        exec_string = ''
-        for v in syms:
-            exec_string += str(v) + ' = model.getVarByName("' + str(v) + '")\n'
-        for key, value in replacement_dict.items():
-            exec_string += f"{value[0]} = model.getVarByName('{value[0]}')\n"
-            exec_string += f"{value[1]} = model.getVarByName('{value[1]}')\n"
-            exec_string += f"model.addConstr({key} == {value[1]})\n"
-            exec_string += f"model.addGenConstrPow({value[1]}, {value[0]}, 0.5)\n"
-        for constr in constraint_strings:
-            exec_string += 'model.addConstr(' + constr + ')\n'
-
-        model.update() # essential to get all information
-
-        exec(exec_string)
+        model = build_gurobi_model_with_square_roots(constraint_strings, syms, replacement_dict, num_aux)
 
         model.write("model.lp")
         model.optimize()
